5dc528a31c1f864e738cc25314d31da84991b236
kqmirasol <kqmirasol@icloud.com>
2024-12-09 12:54:37 +0800

    Directives and routing added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { DataBindingDemoComponent } from './data-binding-demo/data-binding-demo.component';
+import { DirectivesDemoComponent } from './directives-demo/directives-demo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DataBindingDemoComponent],
+  imports: [RouterOutlet, DataBindingDemoComponent,DirectivesDemoComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
